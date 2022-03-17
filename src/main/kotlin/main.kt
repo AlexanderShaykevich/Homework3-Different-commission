47a214f185c575fac194abc04305c985bed5b584
Alexander <9922202@gmail.com>
2022-03-17 17:55:07 +0300

    Change method transfer

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,16 +5,16 @@ const val VISA = "visa"
 const val MIR = "mir"
 const val commissionMasterMaestro = 0.06
 const val commissionVisaMir = 0.075
-const val commissionMinVisaMir = 3500
+const val minCommissionVisaMir = 3500
 const val maxTransferWithoutCommission = 7500000
 const val addCommissionMasterMaestro = 2000
-
 
 fun main() {
     println(transfer(amount = 8000000))
     println(transfer(MASTER, 5000000, 8000000))
     println(transfer(MAESTRO, 7600000, 8000000))
     println(transfer(accountType = MASTER, amount = 8000000))
+    println(transfer(accountType = VISA, amount = 8000000))
     println(transfer(accountType = VISA, amount = 8000000))
     println(transfer(accountType = MIR, amount = 5000))
 }
@@ -26,9 +26,9 @@ fun transfer(accountType: String = VK, previous: Long = 0, amount: Long): Number
             if (previous <= maxTransferWithoutCommission) amount
             else amount - (amount * commissionMasterMaestro) - addCommissionMasterMaestro
         VISA, MIR ->
-            if (amount * commissionMinVisaMir < commissionMinVisaMir) amount - commissionMinVisaMir
+            if (amount * commissionVisaMir < minCommissionVisaMir) amount - minCommissionVisaMir
             else amount - (amount * commissionVisaMir)
-        else -> 0.00
+        else -> 0
     }
     return toTransfer
 }
